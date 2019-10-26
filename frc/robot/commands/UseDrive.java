3fa0e2e730bdbe2fb7d828fac0cfe88402f1d3fd
EmmanuelSaifer <amanuels21@parkschool.net>
2019-10-26 14:00:38 -0400

    seeds of the drive subsystem

--- frc/robot/commands/UseDrive.java
+++ frc/robot/commands/UseDrive.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -8,15 +8,17 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.Robot;
+import frc.robot.subsystems.Drive;
 
-/**
- * An example command.  You can replace me with your own command.
- */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
-    // Use requires() here to declare subsystem dependencies
-    requires(Robot.m_subsystem);
+public class UseDrive extends Command {
+  Drive driveSub;
+
+  public UseDrive(Drive driveSub) {
+    super("UseDrive");
+
+    this.driveSub = driveSub;
+
+    requires(driveSub);
   }
 
   // Called just before this Command runs the first time
@@ -27,6 +29,7 @@ public class ExampleCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    
   }
 
   // Make this return true when this Command no longer needs to run execute()
